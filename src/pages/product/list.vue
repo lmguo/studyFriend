505b1bb49a4700be9ac271571d674841d1ec6ce7
lmguo <806793924@qq.com>
2020-01-04 13:09:07 +0800

    finish all router

--- src/pages/product/list.vue
+++ src/pages/product/list.vue
@@ -1,23 +1,22 @@
 <template>
     <div>
-        <h3>产品管理<br></h3>
         <el-button type="primary" @click="toAddHandler" size="small">添加</el-button>
         <el-button type="danger" size="small">批量删除</el-button>
 
-        <el-table ref="multipleTable" :data="product" tooltip-effect="dark" stripe>
+        <el-table ref="multipleTable" :data="product" tooltip-effect="dark" stripe size="small" height="1000px">
             <el-table-column type="selection"></el-table-column>
-            <el-table-column prop="id" label="编号">
+            <el-table-column prop="id" label="编号" width="200">
                 <!-- <template slot-scope="scope">{{ scope.row.date }}</template> -->
             </el-table-column>
             <el-table-column prop="name" label="产品名称" width="240"></el-table-column>
             <el-table-column prop="price" label="价格" width="240" show-overflow-tooltip></el-table-column>
             <el-table-column prop="description" label="描述" width="240" show-overflow-tooltip></el-table-column>
-            <el-table-column prop="categoryId" label="所属产品" width="240" show-overflow-tooltip></el-table-column>
+            <el-table-column prop="categoryId" label="所属产品" width="200" show-overflow-tooltip></el-table-column>
             <el-table-column prop="address" label="操作" width="240" show-overflow-tooltip>
                 <template v-slot="slot">
                     <i @click="upDataHandler(slot.row)" class="el-icon-edit-outline" ></i>
                     <i @click="deleteHandler(slot.row.id)" class="el-icon-delete" ></i>
-                    <!-- <el-button >详情</el-button> -->
+                    <a href="" >详情</a>
                 </template>
             </el-table-column>
         </el-table>
@@ -26,9 +25,9 @@
             :visible.sync="visible"
             width="60%">
              <el-form :model="ruleform" :rules="rules" ref="ruleform" label-width="80px">
-                <el-form-item prop="id" label="编号">
+                <!-- <el-form-item prop="id" label="编号">
                     <el-input v-model="ruleform.id"></el-input>
-                </el-form-item>
+                </el-form-item> -->
                 <el-form-item prop="name" label="产品名称">
                     <el-input v-model="ruleform.name"></el-input>
                 </el-form-item>
@@ -39,7 +38,14 @@
                     <el-input v-model="ruleform.description"></el-input>
                 </el-form-item>
                 <el-form-item prop="categoryId" label="所属类别">
-                    <el-input v-model="ruleform.categoryId"></el-input>
+                    <el-select v-model="value" placeholder="请选择">
+                        <el-option
+                            v-for="item in options"
+                            :key="item.value"
+                            :label="item.label"
+                            :value="item.value">
+                        </el-option>
+                    </el-select>
                 </el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
@@ -55,26 +61,33 @@ import querystring from 'querystring'
 export default {
     data(){
         return{
+            options: [{
+                value: '选项1',
+                label: '9139'
+                }, {
+                value: '选项2',
+                label: '9202'
+                }, {
+                value: '选项3',
+                label: '9357'
+                }, {
+                value: '选项4',
+                label: '9358'
+                }, {
+                value: '选项5',
+                label: '9392'
+                }],
+                value: '',
             product:[],
             ruleform:{},
             visible:false,
-            delivery: false,
+            
             rules:{
-                id: [
-                { required: true, message: '请输入产品编号', trigger: 'blur' }
-                ],
                 name: [
                 { required: true, message: '请输入产品名称', trigger: 'blur' }
                 ],
                 price: [
                 { required: true, message: '请输入产品价格', trigger: 'blur' }
-                ],
-                description: [
-                { required: true, message: '请输入产品描述', trigger: 'blur' }
-                ],
-                categoryId: [
-                { required: true, message: '请输入所属类别序号', trigger: 'blur' }
-                
                 ],
             }
 
